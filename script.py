bd3739c7babffb1cd019f289391156f5e28d335b
nandhunair1 <77907590+nandhunair1@users.noreply.github.com>
2021-03-07 23:55:22 +0530

    Update script.py

--- script.py
+++ script.py
@@ -26,12 +26,12 @@ No need add each filter again!
 Bot will automatically search for your file and give links to that!
 </i>
 
-<b>© @TroJanzHEX</b>"""
+<b>© @MrC_VENOM</b>"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @MrC_VENOM
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
